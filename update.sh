a2d8c41b0e1ff4af6dd6de28c5f20a99b77f3a93
Rafael Cecilio <62517727+rafae1cecilio@users.noreply.github.com>
2025-04-26 14:12:40 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo -S purge &&
+sudo -u $USER brew analytics off
 sudo -u $USER brew update &&
 sudo -u $USER brew upgrade &&
 sudo -u $USER brew update &&
@@ -10,6 +11,20 @@ sudo -u $USER brew autoremove &&
 sudo -S dscacheutil -flushcache &&
 sudo -S killall -HUP mDNSResponder &&
 sudo -S purge &&
-touch /Users/$USER/.ssh/known_hosts | echo "oi" >> /Users/$USER/.ssh/known_hosts | rm /Users/$USER/.ssh/known_hosts &&
-sudo -S softwareupdate -i -a -R --agree-to-license --verbose 
-
+echo "" > /Users/$USER/.ssh/known_hosts &&
+#sudo -S softwareupdate -i -a -R --agree-to-license --verbose 
+sudo scutil --set ComputerName M1
+sudo scutil --set LocalHostName M1
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/thumbnails.fraghandler
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/exclusive
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/index.sqlite
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/index.sqlite-shm
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/index.sqlite-wal
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/resetreason
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/thumbnails.data
+sudo rm -rfv $(getconf DARWIN_USER_CACHE_DIR)/com.apple.QuickLook.thumbnailcache/thumbnails.fraghandler
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write com.apple.finder AppleShowAllFiles -bool true
+chflags nohidden ~/Library
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
