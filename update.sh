33f86731a33725c2bea457bb13b3e1abf213aae1
Real Roboto <62517727+realroboto@users.noreply.github.com>
2026-03-02 11:55:07 -0500

    Remove command to disable transparency
    Removed the command to disable transparency in accessibility settings.

--- update.sh
+++ update.sh
@@ -42,9 +42,6 @@ defaults write com.apple.assistant.support "Assistant Enabled" -bool false
 defaults write com.apple.assistant.support "Assistant Enabled" -bool false
 defaults write com.apple.Siri StatusMenuVisible -bool false
 
-# Desabilita transparências
-#defaults write com.apple.universalaccess reduceTransparency -bool true
-
 #Acelerar Animações do Finder
 defaults write com.apple.finder DisableAllAnimations -bool true
 
